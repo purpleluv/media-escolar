5a6f45ddf974f93aa7384f24de1b62bfafb4a6b4
lucas <purpleboostup@gmail.com>
2022-12-15 01:13:06 -0300

    Tudo pronto.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 nome = input('Digite o seu nome: ')
-print(f'Olá {nome}, seja bem vindo(a)!')
+print(f'Olá {nome}, seja bem vindo(a)!\n')
 
 n1 = int(input(f'{nome}, digite a nota que você tirou na primeira prova: '))
-n2 = int(input('Agora, digite a nota que você tirou na segunda prova: '))
-n3 = int(input('Digite a nota que você ganhou no trabalho: '))
+n2 = int(input('\nAgora, digite a nota que você ganhou no trabalho: '))
+n3 = int(input('\nDigite a nota da segunda prova: '))
 
-soma = n1 + n2 + n3 / 3
+soma = (n1 + n2 + n3) / 2
 
 print(f'{nome}, sua média é {soma}.')
+
+input('\nAperte enter para fechar esta janela ')
