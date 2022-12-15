727870e8a44d47a16718d251adbbdfb4e200efa7
lucas <purpleboostup@gmail.com>
2022-12-15 00:40:10 -0300

    Fim do projeto

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ n1 = int(input(f'{nome}, digite a nota que você tirou na primeira prova: '))
 n2 = int(input('Agora, digite a nota que você tirou na segunda prova: '))
 n3 = int(input('Digite a nota que você ganhou no trabalho: '))
 
-média = n1 + n2 + n3 / 3 
+soma = n1 + n2 + n3 / 3
 
-if média > 6:
-    print(f'{nome}, sua média é {média}, parabéns, você está aprovado(a)!')
-else:
-    print(f'{nome}, sua média é {média}, infelizmente você está reprovado...')
+print(f'{nome}, sua média é {soma}.')
